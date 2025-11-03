55601f5e8f269464ae1888f055829c09bc9bd1d6
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2025-11-03 18:07:21 +0530

    done

---
+++ ORSProject-04/src/main/java/in/co/rays/proj4/bean/CourseBean.java
@@ -0,0 +1,32 @@
+package in.co.rays.proj4.bean;
+
+public class CourseBean {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	private String duration;
+	private String description;
+}
